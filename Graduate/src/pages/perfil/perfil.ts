36c958faf4a3715a48a450eda533c4adee1c1c69
yamilaBacchini <yamicqq@hotmail.com>
2018-06-24 19:11:24 -0300

    cosas

--- Graduate/src/pages/perfil/perfil.ts
+++ Graduate/src/pages/perfil/perfil.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, DateTime } from 'ionic-angular';
 
 @Component({
   selector: 'page-perfil',
   templateUrl: 'perfil.html'
 })
 export class PerfilPage {
+  editar: boolean = false;
+  valueNombre: string = '';
+  valueTelefono: string = '';
+  valueMail: string = '';
+  valueFechaNacimiento: Date = new Date();
+  valueDescripcion: string = '';
   // this tells the tabs component which Pages
   // should be each tab's root Page
   constructor(public navCtrl: NavController) {
   }
+
+  onClickEditar() {
+    this.editar = !this.editar;
+  }
+
   
 }
